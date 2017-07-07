c952e888bc5b73e6e519dce2138fd17f34b6f5e5
jeffersondc10 <jeffersondavid0610@gmail.com>
2017-07-07 17:03:04 -0500

    Se realiza la eliminación de dulces y muestra de puntos en el tablero derecho

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var eliminar = 0;
 var seg = 0;
 var min = 2;
 var tiempo = 0;
+var score = 0;
 
 //---------Iteración de color en el titulo----------//
 function tituloBlanco(elemento){
@@ -55,10 +56,10 @@ function timer(){
 		seg=seg-1;}
 	if(seg==0){
 		if(min==0){
-			//clearInterval(eliminar);
+			clearInterval(eliminar);
 			//clearInterval(nuevosDulces);
 			clearInterval(intervalo);
-			//clearInterval(tiempo);
+			clearInterval(tiempo);
 			$(".panel-tablero").hide("drop","slow",funcioncita);
 			$(".time").hide();
       $(".panel-score").show();
@@ -68,23 +69,131 @@ function timer(){
 	$("#timer").html("0"+min+":"+seg);
 };
 
+//----Muestra el score mas grande
 function funcioncita(){
 	$( ".panel-score" ).animate({width:'100%'},3000);
-	$(".termino").css({"display":"block","text-align":"center"});
+	//$(".termino").css({"display":"block","text-align":"center"});
 };
 
+//---Limpia el tablero
+function borrartotal(){
+	for(var j=1;j<8;j++){
+		$(".col-"+j).children("img").detach();}
+};
 
+//-----Función para eliminar los dulces cuando hay combinación de 3 o mas
+function eliminarhorver(){
+	matriz=0;
+	busquedaHorizontal=horizontal();
+	busquedaVertical=vertical();
+	for(var j=1;j<8;j++){
+		matriz=matriz+$(".col-"+j).children().length;}
+	//Condicional si no encuentra 3 dulces o más, llamamos a la función para volver a completar el juego
+	if(busquedaHorizontal==0 && busquedaVertical==0 && matriz!=49){
+		clearInterval(eliminar);
+		buscarNuevosDulces=0;
+		nuevosDulces=setInterval(function(){
+			nuevosdulces()
+		},600);}
+
+	if(busquedaHorizontal==1||busquedaVertical==1){
+		$(".elemento").draggable({disabled:true});
+		$("div[class^='col']").css("justify-content","flex-end");
+		$(".activo").hide("pulsate",1000,function(){
+			var scoretmp=$(".activo").length;
+			$(".activo").remove("img");
+			score=score+scoretmp*10;
+			$("#score-text").text(score);//Cambiamos la puntuación
+		});
+	}
+	if(busquedaHorizontal==0 && busquedaVertical==0 && matriz==49){
+		$(".elemento").draggable({
+			disabled:false,
+			containment:".panel-tablero",
+			revert:true,
+			revertDuration:0,
+			snap:".elemento",
+			snapMode:"inner",
+			snapTolerance:40,
+			start:function(event,ui){
+				mov=mov+1;
+				$("#movimientos-text").html(mov);}
+		});
+	}
+	$(".elemento").droppable({
+		drop:function (event,ui){
+			var dropped=ui.draggable;
+			var droppedOn=this;
+			espera=0;
+			do{
+				espera=dropped.swap($(droppedOn));}
+			while(espera==0);
+			busquedaHorizontal=horizontal();
+			busquedaVertical=vertical();
+			if(busquedaHorizontal==0 && busquedaVertical==0){
+				dropped.swap($(droppedOn));}
+			if(busquedaHorizontal==1 || busquedaVertical==1){
+				clearInterval(nuevosDulces);
+				clearInterval(eliminar);
+				eliminar=setInterval(function(){
+					eliminarhorver()
+				},150);}},
+	});
+};
+
+//----Función para la busqueda horizontal de dulces
+function horizontal(){
+	var busHori=0;
+	for(var j=1;j<8;j++){
+		for(var k=1;k<6;k++){
+			var res1=$(".col-"+k).children("img:nth-last-child("+j+")").attr("src");
+			var res2=$(".col-"+(k+1)).children("img:nth-last-child("+j+")").attr("src");
+			var res3=$(".col-"+(k+2)).children("img:nth-last-child("+j+")").attr("src");
+			if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null)){
+				$(".col-"+k).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				$(".col-"+(k+1)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				$(".col-"+(k+2)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				busHori=1;
+			}
+		}
+	}
+	return busHori;
+};
+
+//----------Función para la busqueda vertical de dulces------------------------------
+function vertical(){
+	var busVerti=0;
+	for(var l=1;l<6;l++){
+		for(var k=1;k<8;k++){
+			var res1=$(".col-"+k).children("img:nth-child("+l+")").attr("src");
+			var res2=$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("src");
+			var res3=$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("src");
+			if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null)){
+				$(".col-"+k).children("img:nth-child("+(l)+")").attr("class","elemento activo");
+				$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("class","elemento activo");
+				$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("class","elemento activo");
+				busVerti=1;
+			}
+		}
+	}
+	return busVerti;
+};
 
 $(function(){
   tituloBlanco(".main-titulo")
   $(".btn-reinicio").on("click", function(){
+    i=0;
+    $(this).text("Reiniciar")
+    clearInterval(intervalo);
+    clearInterval(tiempo);
+    min = 2;
+    seg = 0;
+    borrartotal()
     intervalo=setInterval(function(){
   		desplazamiento()
   	},600);
     tiempo = setInterval(function(){
       timer()
     },1000);
-    $(this).text("Reiniciar")
-
   });
 });
