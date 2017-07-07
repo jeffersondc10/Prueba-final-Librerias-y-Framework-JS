5d83937d4136eb5e7a0c3265b97151217d5869eb
jeffersondc10 <jeffersondavid0610@gmail.com>
2017-07-07 12:16:57 -0500

    Se realizó el temporizador de dos minutos con su consecuencia de ocultación del tablero de juego y el show del score en toda la pantalla

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var numero = 0;
 var imagen = 0;
 var intervalo = 0;
 var eliminar = 0;
+var seg = 0;
+var min = 2;
+var tiempo = 0;
 
 //---------Iteración de color en el titulo----------//
 function tituloBlanco(elemento){
@@ -25,6 +28,8 @@ function tituloAmarillo(elemento){
   )
 }
 
+
+//----Función para rellenar las columnas con dulces aleatorios
 function desplazamiento(){
 	i=i+1
 	var numero=0;
@@ -44,10 +49,42 @@ function desplazamiento(){
 	},150);}
 };
 
+//---temporizador de dos minutos---
+function timer(){
+	if(seg!=0){
+		seg=seg-1;}
+	if(seg==0){
+		if(min==0){
+			//clearInterval(eliminar);
+			//clearInterval(nuevosDulces);
+			clearInterval(intervalo);
+			//clearInterval(tiempo);
+			$(".panel-tablero").hide("drop","slow",funcioncita);
+			$(".time").hide();
+      $(".panel-score").show();
+    }
+		seg=59;
+		min=min-1;}
+	$("#timer").html("0"+min+":"+seg);
+};
+
+function funcioncita(){
+	$( ".panel-score" ).animate({width:'100%'},3000);
+	$(".termino").css({"display":"block","text-align":"center"});
+};
 
 
 
 $(function(){
   tituloBlanco(".main-titulo")
+  $(".btn-reinicio").on("click", function(){
+    intervalo=setInterval(function(){
+  		desplazamiento()
+  	},600);
+    tiempo = setInterval(function(){
+      timer()
+    },1000);
+    $(this).text("Reiniciar")
 
+  });
 });
